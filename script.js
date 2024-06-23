f3cb4ce59ca07417a7722db5489590093594a125
user.name "Dev dheer <user.email "ddheer157@gmail.com>
2024-06-23 13:52:54 +0530

    final 1

--- script.js
+++ script.js
@@ -1,103 +1,91 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
 // loading animation
-function loader() {
-    function showContent() {
-        document.getElementById('loader').style.display = 'none';
-        document.getElementById('main').style.display = 'block';
-    }
+// function loader() {
+//     function showContent() {
+//         document.getElementById('loader').style.display = 'none';
+//         document.getElementById('main').style.display = 'block';
+//     }
 
 
-    window.addEventListener('load', function () {
-        setTimeout(showContent, 0);
-    });
+//     window.addEventListener('load', function () {
+//         setTimeout(showContent, 0);
+//     });
 
-    let loadertl = gsap.timeline()
-
-    loadertl.to("#main", {
-        display: "none",
-        duration: 0.1,
-        delay: -1
-    })
-
-    loadertl.to(".bg-loader", {
-        bottom: "0%",
-        height: "100%",
-        duration: 0.4,
-        delay: 0.8
-    })
-
-    loadertl.from(".text-loader h1 span", {
-        opacity: 0,
-        x: 20,
-        stagger: 0.19,
-        duration: 0.5
-    })
-
-    loadertl.to(".text-loader h1 span", {
-        opacity: 0,
-        y: -20,
-        stagger: 0.1,
-        duration: 0.2,
-        delay: 0.3
-    })
-
-    loadertl.to(".bg-loader", {
-        bottom: "100%",
-        height: "0%",
-        duration: 0.4,
-        oncomplete: function () {
-            animatehomepage();
-        }
-    })
-}
-loader()
-
-// home page animation
-// function animatehomepage() {
-//     let hometl = gsap.timeline()
-//     hometl.from(".logo", {
-//         y: -100,
-//         duration: 0.5,
-//         opacity: 0,
-//         delay: 1, 
-//     })
-//     hometl.from('.nav-partition', {
-//         opacity:0
-//     })
-//     hometl.from(".links li, #nav-bag", {
-//         y: -40,
-//         stagger: 0.1,
-//         opacity: 0,
+//     let loadertl = gsap.timeline()
+//     loadertl.to(".bg-loader", {
+//         bottom: "0%",
+//         height: "100%",
 //         duration: 0.4,
+//         delay: 0.8
 //     })
-//     hometl.from('.left-home h1 span', {
-//         y: 50,
-//         duration: 0.6,
+
+//     loadertl.from(".text-loader h1 span", {
+//         opacity: 0,
+//         x: 20,
+//         stagger: 0.19,
+//         duration: 0.5
+//     })
+
+//     loadertl.to(".text-loader h1 span", {
+//         opacity: 0,
+//         y: -20,
 //         stagger: 0.1,
-//         opacity: 0,
-//         delay: -0.9,
+//         duration: 0.2,
+//         delay: 0.3
 //     })
-//     hometl.from('.btn', {
-//         x: -50,
+
+//     loadertl.to(".bg-loader", {
+//         bottom: "100%",
+//         height: "0%",
 //         duration: 0.4,
-//         opacity: 0,
-//     })
-//     hometl.from('.right-home', {
-//         x: 200,
-//         duration: 0.6,
-//         opacity: 0,
-//         delay: -0.2,
-//     })
-//     hometl.from('.home-beans', {
-//         x: -100,
-//         duration: 0.6,
-//         opacity: 0,
-//         delay: -0.4,
+//         oncomplete: function () {
+//             animatehomepage();
+//         }
 //     })
 // }
+
+// home page animation
+function animatehomepage() {
+    let hometl = gsap.timeline()
+    hometl.from(".logo", {
+        y: -100,
+        duration: 0.5,
+        opacity: 0,
+        delay: 1, 
+    })
+    hometl.from('.nav-partition', {
+        opacity:0
+    })
+    hometl.from(".links li, #nav-bag", {
+        y: -40,
+        stagger: 0.1,
+        opacity: 0,
+        duration: 0.4,
+    })
+    hometl.from('.left-home h1 span', {
+        y: 50,
+        duration: 0.6,
+        stagger: 0.1,
+        opacity: 0,
+        delay: -0.9,
+    })
+    hometl.from('.btn', {
+        x: -50,
+        duration: 0.4,
+        opacity: 0,
+    })
+    hometl.from('.right-home', {
+        x: 200,
+        duration: 0.6,
+        opacity: 0,
+        delay: -0.2,
+    })
+    hometl.from('.home-beans', {
+        x: -100,
+        duration: 0.6,
+        opacity: 0,
+        delay: -0.4,
+    })
+}
 // Navbar Dropdown
 function dropdown() {
     let droptl = gsap.timeline()
@@ -133,8 +121,8 @@ function dropdown() {
         })
     })
 
-}
-dropdown()
+};
+dropdown();
 
 // navbar responsive
 function navresponsive() {
@@ -168,7 +156,7 @@ function navresponsive() {
     navclose.addEventListener('click', () => {
         navcontent.classList.remove('active')
         if (navcontent.classList.contain('active')) {
-            
+
             console.log('there is active class')
         }
         else {
@@ -189,117 +177,272 @@ function navresponsive() {
             })
         }
     })
-}
-navresponsive()
+};
+navresponsive();
 
 // cursor follower
 function mousefollower() {
-    const content = document.querySelectorAll('.left-home, .btn, .right-home')
+    const content = document.querySelectorAll('.left-home h1, .btn, .home-img, .home-beans')
     const content2 = document.querySelectorAll(".links li")
     const cursor = document.querySelector(".cursor-follower")
-    const page1 = document.querySelector(".home")
-
-    window.addEventListener("mousemove", (e) => {
-        gsap.to(cursor, {
-            duration: 0.2,
-            opacity: 1,
-            x: e.clientX - 1,
-            y: e.clientY - 1,
-            delay: 0.1
-        })
-    })
+    const cards = document.querySelectorAll('.highlight, .product, .left-sec-3 img, .sec-7 img')
 
 
-    page1.addEventListener("mouseleave", () => {
-        gsap.to(cursor, {
-            opacity: 0,
-        })
-    })
-    // content hovering
-    content.forEach(link => {
-        link.addEventListener("mouseenter", () => {
+    document.addEventListener('DOMContentLoaded', () => {
+        document.addEventListener('mousemove', (e) => {
+
             gsap.to(cursor, {
-                width: "80px",
-                height: "80px",
+                x: e.clientX - 2,
+                y: e.clientY + 10,
+                duration: 0.3,
+                opacity: 1,
+            });
+            gsap.to('.cursor-follower p, .cursor-follower svg', {
+                display: 'none',
+                duration: 0.1
+            });
+        });
+    });
+
+    // content hovering
+    content.forEach(contentHovering => {
+        contentHovering.addEventListener("mouseenter", () => {
+            gsap.to(cursor, {
+                width: "50px",
+                height: "50px",
                 backgroundColor: "#FFF6DD",
+                border: "1px solid #513206",
                 duration: 0.2,
                 mixBlendMode: 'difference',
-            })
-        })
-    })
+            });
+        });
+    });
 
-    content.forEach(link => {
-        link.addEventListener("mouseleave", () => {
+    content.forEach(contentHovering => {
+        contentHovering.addEventListener("mouseleave", () => {
             gsap.to(cursor, {
-                width: "40px",
-                height: "40px",
-                backgroundColor: "transparent",
+                width: "15px",
+                height: "15px",
+                backgroundColor: "#513026",
+                border: "none",
                 duration: 0.2,
                 mixBlendMode: 'normal',
-            })
-        })
-    })
+            });
+            gsap.to('.cursor-follower p, .cursor-follower svg', {
+                display: 'none',
+                duration: 0.1
+            });
+        });
+    });
 
 
     // links hovering
 
-    content2.forEach(link => {
-        link.addEventListener("mouseenter", () => {
-            gsap.to(cursor, {
-                width: "60px",
-                height: "60px",
-                backgroundColor: "#fff6dd",
-                duration: 0.2,
-                mixBlendMode: 'difference',
-            })
-        })
-    })
-
-    content2.forEach(link => {
-        link.addEventListener("mouseleave", () => {
+    content2.forEach(linkHovering => {
+        linkHovering.addEventListener("mouseenter", () => {
             gsap.to(cursor, {
                 width: "40px",
                 height: "40px",
-                backgroundColor: "transparent",
+                backgroundColor: "#fff6dd",
+                border: "1px solid #513026",
+                duration: 0.2,
+                mixBlendMode: 'difference',
+            });
+        });
+    });
+
+    content2.forEach(linkHovering => {
+        linkHovering.addEventListener("mouseleave", () => {
+            gsap.to(cursor, {
+                width: "15px",
+                height: "15px",
+                backgroundColor: "#513026",
+                border: "none",
                 duration: 0.2,
                 mixBlendMode: 'normal',
-            })
-        })
-    })
-}
-mousefollower()
+            });
+            gsap.to('.cursor-follower p, .cursor-follower svg', {
+                display: 'none',
+                duration: 0.1
+            });
+        });
+    });
+
+
+    // HIghlights and produxt cards hovering
+
+    cards.forEach(cardHovering => {
+        cardHovering.addEventListener("mouseenter", () => {
+            gsap.to(cursor, {
+                width: "50px",
+                height: "50px",
+                duration: 0.2,
+            });
+            gsap.to('.cursor-follower p, .cursor-follower svg', {
+                display: 'block',
+            });
+        });
+    });;
+
+    cards.forEach(cardHovering => {
+        cardHovering.addEventListener("mouseleave", () => {
+            gsap.to(cursor, {
+                width: "15px",
+                height: "15px",
+                backgroundColor: "#513026",
+                border: "none",
+                duration: 0.2,
+                mixBlendMode: 'normal',
+            });
+            gsap.to('.cursor-follower p, .cursor-follower svg', {
+                display: 'none',
+                delay: -0.9,
+            });
+        });
+    });
+};
+mousefollower();
 
 // sec-2 marquee
-
-window.addEventListener('wheel', function(dets){
-    if(dets.deltaY<0){
-        gsap.to('.marquee', {
-            transform: 'translateX(-200%)',
-            duration: 2,
-            repeat: -1,
-            ease:'none'
-        })
-    }
-    else{
-        gsap.to('.marquee', {
-            transform: 'translateX(0%)',
-            duration: 2,
-            repeat: -1,
-            ease:'none'
-        })
-    }
-})
-
+function marqueeheading() {
+    window.addEventListener('wheel', function (dets) {
+        if (dets.deltaY < 0) {
+            gsap.to('.marquee', {
+                transform: 'translateX(-200%)',
+                duration: 2,
+                repeat: -1,
+                ease: 'none'
+            })
+        }
+        else {
+            gsap.to('.marquee', {
+                transform: 'translateX(0%)',
+                duration: 2,
+                repeat: -1,
+                ease: 'none'
+            })
+        }
+    });
+};
+marqueeheading();
 // gsap scroll trigger
+function scrolltrigger() {
 
-gsap.from('.one-high', {
-    scrollTrigger:{
-        trigger:"h1",
-        start:"top 20%", 
-        end: "top  0%",
-        markers: true,
-    },
-    y:90,
-    duration: 0.8,
-    opacity: 0
-})
+    gsap.to(".horizontal-container", {
+        xPercent: -80,
+        scrollTrigger: {
+            trigger: ".horizontal-container",
+            scrub: 1,
+            end: "+=3000",
+            pin: true
+        }
+    });
+
+
+    ScrollTrigger.refresh();
+
+
+    // tiggle special scrolltrigger
+    gsap.from(".sec-1 h1, .sec-1 p, .sec-1 .btn", {
+        scrollTrigger: {
+            trigger: ".sec-1",
+            scrub: 1,
+            start: 'top 30%',
+            end: "top 100%",
+        },
+        y: 30,
+        opacity: 0,
+        stagger: 0.1,
+        ease: "expo.out",
+    });
+
+    //   highlight scrolltrigger
+    gsap.from(".highlight", {
+        scrollTrigger: {
+            trigger: ".sec-2",
+            scrub: 1,
+            start: 'top 30%',
+            end: 'top 100%',
+        },
+        y: 30,
+        opacity: 0,
+        stagger: 0.2
+    });
+
+    //   about best seller scrolltrigger
+    gsap.from(".left-sec-3, .sec-3 #heading, .right-sec-3 h1, .right-sec-3 p, .right-sec-3 .btn", {
+        scrollTrigger: {
+            trigger: ".sec-3",
+            scrub: 1,
+            start: 'top 30%',
+            end: 'top 100%',
+        },
+        y: 30,
+        opacity: 0,
+        stagger: 0.2
+    });
+
+    //   products scrolltrigger
+    gsap.from(".sec-4 #heading, .product, #next", {
+        scrollTrigger: {
+            trigger: ".sec-4",
+            scrub: 1,
+            start: 'top 30%',
+            end: 'top 100%',
+        },
+        y: 30,
+        opacity: 0,
+        stagger: 0.2
+    });
+    //as seen on scrolltrigger 
+    gsap.from(".row-1 img, .row-2 img, .sec-7 #heading", {
+        scrollTrigger: {
+            trigger: ".sec-7",
+            scrub: 1,
+            start: 'top 30%',
+            end: 'top 100%',
+        },
+        y: 30,
+        opacity: 0,
+        stagger: 0.2,
+        duration: 1
+    });
+    //our story scrolltrigger
+    gsap.from(".sec-6 #heading, .sec-6 #text, .bg-image-sec6", {
+        scrollTrigger: {
+            trigger: ".sec-6",
+            scrub: 1,
+            start: 'top 30%',
+            end: 'top bottom',
+        },
+        y: 40,
+        opacity: 0,
+        stagger: 0.2,
+        duration: 9
+    }); 
+    gsap.from(".one-story, .three-story", {
+        scrollTrigger: {
+            trigger: ".sec-6",
+            scrub: 1,
+            start: 'top 30%',
+            end: 'top bottom',
+        },
+        x: -200,
+        opacity: 0,
+        stagger: 0.2,
+        duration: 9
+    });
+    gsap.from(".two-story, .four-story", {
+        scrollTrigger: {
+            trigger: ".sec-6",
+            scrub: 1,
+            start: 'top 30%',
+            end: 'top bottom',
+        },
+        x: 200,
+        opacity: 0,
+        stagger: 0.2,
+        duration: 9
+    });
+};
+scrolltrigger()
